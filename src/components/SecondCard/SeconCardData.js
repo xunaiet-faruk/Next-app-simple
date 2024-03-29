de6f4f9e665077b62d8c5ae7a5e3c2dc2ff277e6
xunaiet-faruk <xunaiet28@gmail.com>
2024-03-30 04:35:46 +0600

    al page responsive mobaile,tab,laptop

--- src/components/SecondCard/SeconCardData.js
+++ src/components/SecondCard/SeconCardData.js
@@ -1,85 +1,72 @@
-
 import Image from 'next/image';
-import Icon from '../../../public/IconRight.png'
-import image1 from '../../../public/player1.png'
-import icon2 from '../../../public/Vector (1).png'
-import CradImage from '../../../public/Ticket_Mockup 3.png'
-import CradImage2 from '../../../public/unnamed 1.png'
+import Icon from '../../../public/IconRight.png';
+import image1 from '../../../public/player1.png';
+import icon2 from '../../../public/Vector (1).png';
+import CradImage from '../../../public/Ticket_Mockup 3.png';
+import CradImage2 from '../../../public/unnamed 1.png';
+import Background from '../../../public/Background.png';
+
 const SeconCardData = () => {
     return (
-        <div className='flex justify-around'>
-            <div className='border-2 h-12 border-[#2C9CF0] w-8 mt-[200px]'>
+        <div className='flex flex-col justify-around md:flex-row'>
+            <div className='border-2 h-12 border-[#2C9CF0] w-8 mt-2 md:mt-0'>
                 <Image className='ml-2 mt-3' src={Icon} alt='icon' />
             </div>
 
-
-            {/* card hereee  */}
-            <div className='grid lg:grid-cols-3 gap-5 md:grid-cols-2 grid-cols-1'>
-                <div className="flex justify-center md:w-auto items-center pt-2 shadow-xl px-2">
-                    <div>
-                        <div className="image flex items-center flex-col">
-                            <Image src={CradImage} alt="card" className="w-full h-[400px] object-cover" />
-                            <h2 className="text-lg   text-black capitalize font-semibold py-1 text-center">Las Vegas Aviators</h2>
+            {/* Cards */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center max-w-screen-xl mx-auto'>
+                {/* Card 1 */}
+                <div className='relative'>
+                    <Image src={Background} alt='background' />
+                    <div className='absolute bottom-0'>
+                        <div className="image flex justify-center items-center flex-col">
+                            <Image src={CradImage} alt="card" className="w-full p-1 h-[450px] mb-12 object-cover" />
+                            <h2 className="text-lg text-black capitalize font-semibold py-1 text-center">Las Vegas Aviators</h2>
                         </div>
-                       
-                            <div className='space-y-1'>
-                                <p className="text-sm text-center text-black">Oct 15 |Sun| 4:30 PM</p>
-                                <p className='opacity-60 text-center'>Las Vegas Ballpark, Las Vegas,</p>
-                                <p className='opacity-60 text-center pb-8'> Nevada</p>
-                            </div>
-                            
-                        
-                    </div>
-                </div>
-
-
-                <div className="flex justify-center md:w-auto items-center pt-2 shadow-xl px-2">
-                    <div>
-                        <div className="image flex items-center flex-col">
-                            <Image src={CradImage2} alt="card" className="w-full h-[400px] object-cover" />
-                            <h2 className="text-lg   text-black capitalize font-semibold py-1 text-center">Sacramento River Cats</h2>
-                        </div>
-
                         <div className='space-y-1'>
                             <p className="text-sm text-center text-black">Oct 15 |Sun| 4:30 PM</p>
                             <p className='opacity-60 text-center'>Las Vegas Ballpark, Las Vegas,</p>
-                            <p className='opacity-60 text-center pb-8'> Nevada</p>
+                            <p className='opacity-60 text-center pb-8'>Nevada</p>
                         </div>
-
-
                     </div>
                 </div>
 
-
-
-
-
-                <div className="flex justify-center md:w-auto items-center pt-2 shadow-xl px-2">
-                    <div>
-                        <div className="image flex items-center flex-col">
-                            <Image src={CradImage} alt="card" className="w-full h-[400px] object-cover" />
-                            <h2 className="text-lg   text-black capitalize font-semibold py-1 text-center">Las Vegas Aviators</h2>
+                {/* Card 2 */}
+                <div className='relative'>
+                    <Image src={Background} alt='background' />
+                    <div className='absolute bottom-0'>
+                        <div className="image flex justify-center items-center flex-col">
+                            <Image src={CradImage2} alt="card" className="w-full p-1 h-[450px] mb-12  object-cover" />
+                            <h2 className="text-lg text-black capitalize font-semibold py-1 text-center">Sacramento River Cats</h2>
                         </div>
-
                         <div className='space-y-1'>
                             <p className="text-sm text-center text-black">Oct 15 |Sun| 4:30 PM</p>
                             <p className='opacity-60 text-center'>Las Vegas Ballpark, Las Vegas,</p>
-                            <p className='opacity-60 text-center pb-8'> Nevada</p>
+                            <p className='opacity-60 text-center pb-8'>Nevada</p>
                         </div>
-
-
                     </div>
                 </div>
 
-               
-
-
+                {/* Card 3 */}
+                <div className='relative'>
+                    <Image src={Background} alt='background' />
+                    <div className='absolute bottom-0'>
+                        <div className="image flex justify-center items-center flex-col">
+                            <Image src={CradImage} alt="card" className="w-full p-1 h-[450px] mb-12  object-cover" />
+                            <h2 className="text-lg text-black capitalize font-semibold py-1 text-center">Las Vegas Aviators</h2>
+                        </div>
+                        <div className='space-y-1'>
+                            <p className="text-sm text-center text-black">Oct 15 |Sun| 4:30 PM</p>
+                            <p className='opacity-60 text-center'>Las Vegas Ballpark, Las Vegas,</p>
+                            <p className='opacity-60 text-center pb-8'>Nevada</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div className='border-2 h-12 border-[#2C9CF0] w-8 mt-[200px]'>
+            <div className='border-2 h-12 border-[#2C9CF0] w-8 mt-2 md:mt-0'>
                 <Image className='ml-3 mt-3' src={icon2} alt='icon' />
             </div>
-
         </div>
     );
 };
--- src/components/SecondCard/SeconCards.js
+++ src/components/SecondCard/SeconCards.js
@@ -1,17 +1,17 @@
-import Image from 'next/image';
-import SecondCardData from './SeconCardData';
+import SeconCardData from "./SeconCardData";
+
 
 const SeconCards = () => {
     return (
         <div >
 
-            <div className='bg-red-300 ' style={{ backgroundImage: `url(${"https://i.ibb.co/Ct3K6c6/Rectangle-105.png"})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '800px' }}>
+            <div className='bg-red-300 lg:h-[900px] ' style={{ backgroundImage: `url(${"https://i.ibb.co/Ct3K6c6/Rectangle-105.png"})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
             <div className='mb-16'>
                     <h1 className='text-[50px] font-bold text-center text-black'>Collection Spotlight</h1>
                     <p className='text-[14px] text-black text-center'>Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable</p>
                     <p className='text-center'> experience. Grab yours today!</p>
                 </div>
-                <SecondCardData/>
+               <SeconCardData/>
             </div>
         </div>
     );
